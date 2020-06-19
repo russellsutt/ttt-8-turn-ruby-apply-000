5bbda79cb3f48820d69490ca4715f17ba13fad99
Russell Sutter <russell.sutter@gmail.com>
2020-06-19 17:58:20 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,50 @@
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8)  && !position_taken?(board, index)
+    return true
+  else
+    return false
+  end
+end
+
+def move(board, index, player="X")
+  if valid_move?(board, index)
+    board[index] = player
+  else
+    "Please enter 1-9"
+  end
+end
+
+board = [" ", " ", " "]
+
+def update_array_at_with(array, index, value)
+  array[index] = value
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  input_to_index(user_input)
+  if valid_move(board, user_input)
+end
