65f48d011b8d0fa23a7d1fc48a12fb9b554fc5a8
Russell Sutter <russell.sutter@gmail.com>
2020-06-19 18:12:28 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -48,6 +48,6 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index)
-    update_array_at_with(array, index, value)
+    update_array_at_with(board, index, value)
   end
 end
