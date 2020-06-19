b552d2e4474614b331cf725ae6fcb3dec49c513e
Russell Sutter <russell.sutter@gmail.com>
2020-06-19 18:23:01 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,8 +39,8 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
-  if valid_move?(board, index)
-    move(board, index, player="X")
+  if valid_move?(index)
+    move(board, index)
     display_board(index)
   else
     turn(board)
