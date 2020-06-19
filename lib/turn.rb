e9d12370e79707ccc7e1bc1b1eedb696ee4ebfaf
Russell Sutter <russell.sutter@gmail.com>
2020-06-19 18:24:09 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-    move(index)
+    move(board, index)
     display_board(board)
   else
     turn(board)
